02cbedd40b38ef92ed438a98c3c3c330c903358e
Antoskuu <antoning.antonin@gmail.com>
2024-01-12 15:12:44 +0100

    update

--- topo_test/script.py
+++ topo_test/script.py
@@ -24,13 +24,42 @@ filename = 'config.cfg'
 with open(filename, 'w') as file:
     pass
 
-fichier_cfg = 'config.cfg'
+
 
 
 # if not os.path.exists(fichier_cfg):
 #     with open(fichier_cfg, 'w') as fichier:
 #         fichier.write("CleSansValeur1\n")
 #         fichier.write("CleSansValeur2\n")
+
+###############debut ecriture
+
+
+
+fichier_cfg = 'config.cfg'
+with open(fichier_cfg, 'a') as fichier:
+ 
+
+    fichier.write("version 15.2\n")
+    fichier.write("service timestamps debug datetime msec\n")
+    fichier.write("service timestamps log datetime msec\n")
+    fichier.write("hostname RR1\n")
+    fichier.write("boot-start-marker\n")
+    fichier.write("boot-end-marker\n")
+    fichier.write("no aaa new-model\n")
+    fichier.write("no ip icmp rate-limit unreachable\n")
+    fichier.write("ip cef\n")
+    fichier.write("no ip domain lookup\n")
+    fichier.write("ipv6 unicast-routing\n")
+    fichier.write("ipv6 cef\n")
+    fichier.write("multilink bundle-name authenticated\n")
+    fichier.write("ip tcp synwait-time 5\n")
+
+
+
+
+
+
 
 
 with open(fichier_cfg, 'a') as fichier:
